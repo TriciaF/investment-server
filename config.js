96ac59e7138182924af9a1b8f285f0ab95ce9ee4
TriciaF <TriciaF@thinkful.com>
2018-04-16 14:27:56 -0400

    modified JWT Secret key

--- config.js
+++ config.js
@@ -3,5 +3,5 @@ exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'https://cencible.netlify.c
 exports.DATABASE_URL = process.env.DATABASE_URL ||'mongodb://TriciaF:tjandsam01@ds147589.mlab.com:47589/investment', 
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://Jc:Asdfasdf1@ds229549.mlab.com:29549/teamthree-test';
 exports.PORT = process.env.PORT || 8080;
-exports.JWT_SECRET = process.env.JWT_SECRET; 
+exports.JWT_SECRET = 'chachaslide'; 
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
