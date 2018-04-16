0ce535bdf9d46e66ba229b6f0c91efc9bfdf8365
TriciaF <TriciaF@thinkful.com>
2018-04-16 17:26:30 -0400

    modified the config

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 'use strict';
 exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'https://censible.netlify.com/api',
-exports.DATABASE_URL = process.env.DATABASE_URL ||'mongodb://TriciaF:tjandsam01@ds147589.mlab.com:47589/investment', 
+exports.DATABASE_URL = 'mongodb://TriciaF:tjandsam01@ds147589.mlab.com:47589/investment', 
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://Jc:Asdfasdf1@ds229549.mlab.com:29549/teamthree-test';
 exports.PORT = process.env.PORT || 8080;
 exports.JWT_SECRET = 'chachaslide'; 
